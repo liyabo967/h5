413f254e2c0a121b9ddfcf7370523df273e1a443
快乐智慧 <liyabo967@163.com>
2017-11-16 21:41:22 +0800

    init

---
+++ assets/script/skill.js
@@ -0,0 +1,23 @@
+cc.Class({
+    extends: cc.Component,
+    properties: {},
+
+    onLoad: function () {
+        cc.log("skill------------onLoad");
+        var clickEventHandler = new cc.Component.EventHandler();
+        clickEventHandler.target = this.node; //这个 node 节点是你的事件处理代码组件所属的节点
+        clickEventHandler.component = "MyComponent";//这个是代码文件名
+        clickEventHandler.handler = "callback";
+        clickEventHandler.customEventData = "foobar";
+
+        var button = node.getComponent(cc.Button);
+        button.clickEvents.push(clickEventHandler);
+    },
+
+    callback: function (event, customEventData) {
+        //这里 event 是一个 Touch Event 对象，你可以通过 event.target 取到事件的发送节点
+        var node = event.target;
+        var button = node.getComponent(cc.Button);
+        //这里的 customEventData 参数就等于你之前设置的 "foobar"
+    }
+});
